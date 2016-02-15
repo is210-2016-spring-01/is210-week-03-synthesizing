34a7b2554f702e43fdf52a5202b9a72e84771037
paisely65 <spscuny@gmail.com>
2016-02-15 05:55:23 +0000

    This task has passed tests

--- task_02.py
+++ task_02.py
@@ -4,3 +4,9 @@
 
 
 import inquisition
+SPANISH = inquisition.SPANISH
+print SPANISH
+print SPANISH.index('Spanish')
+print SPANISH[1:19]
+FLEMISH = SPANISH[:19] + 'Flemish' + SPANISH[26:]
+print FLEMISH
