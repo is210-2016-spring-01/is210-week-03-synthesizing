e97e37ed788488420a05708a0afba56bc1ecebbf
Sylvia Guzikowski <xiran-aneki@live.com>
2016-02-15 08:18:36 +0000

    first two tasks done

--- task_02.py
+++ task_02.py
@@ -4,3 +4,17 @@
 
 
 import inquisition
+
+STRLEN = len('Spanish')
+
+NETSTR = len(inquisition.SPANISH)
+
+TARGETSTR = inquisition.SPANISH.index('Spanish')
+
+ENDTARGETSTR = TARGETSTR + STRLEN
+
+STR_LT = inquisition.SPANISH[0:TARGETSTR]
+
+STR_RT = inquisition.SPANISH[ENDTARGETSTR:NETSTR]
+
+FLEMISH = STR_LT + 'Flemish' + STR_RT
