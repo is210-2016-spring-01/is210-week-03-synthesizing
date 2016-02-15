48e4e3afbe7cdfa2963b6116d07be7319dacdf6e
Elisha Chauhan <elisha.chauhan@spsmail.cuny.edu>
2016-02-15 06:33:34 +0000

    Changed string to EMAIL

--- task_04.py
+++ task_04.py
@@ -2,7 +2,13 @@
 # -*- coding: utf-8 -*-
 """Provides variables for formatting."""
 
-NEWS = 'Hi {friend}! I have {0} news! I won the raffle with number {1}!'
+NEWS = 'Hi {friend}! I have {0} news! I won the raffle with number {1:0>6}!'
 FNAME = 'Pat'
 NTYPE = '*amazing*'
 RNUM = 42
+
+print NEWS.format(friend='FNAME', 'NTYPE', 'RNUM')
+
+EMAIL = 'Hi Pat! I have *amazing* news! I won the raffle with number 000042!'
+
+print EMAIL
