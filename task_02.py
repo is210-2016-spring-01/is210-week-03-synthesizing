69e8470d4f5bafa04037cd282c106adbbebee4f4
Elisha Chauhan <elisha.chauhan@spsmail.cuny.edu>
2016-02-15 06:46:04 +0000

    Added Flemish

--- task_02.py
+++ task_02.py
@@ -4,3 +4,12 @@
 
 
 import inquisition
+
+print inquistion.SPANISH.len('Spanish')
+
+print inquisiton[0:6] + inquistion.index('Flemish')
+
+
+FLEMISH = "Nobody expects the Flemish Inquisition! Our chief weapon is surprise......."
+
+print FLEMISH
