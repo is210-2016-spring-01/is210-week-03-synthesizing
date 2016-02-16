f64ab1406572fd404258c09063484bbde674be40
Daniella Sapozhnikova <daniellasapo@gmail.com>
2016-02-16 09:18:27 +0000

    working

--- task_04.py
+++ task_04.py
@@ -2,7 +2,11 @@
 # -*- coding: utf-8 -*-
 """Provides variables for formatting."""
 
-NEWS = 'Hi {friend}! I have {0} news! I won the raffle with number {1}!'
+NEWS = 'Hi{friend}! I have {0} news! I won the raffle with number {1:.6f}!'
 FNAME = 'Pat'
 NTYPE = '*amazing*'
 RNUM = 42
+
+EMAIL = NEWS.format(FNAME, NTYPE, RNUM)
+
+print EMAIL
