626af8cf515a19074724f97d58bde896bbca2dd7
Daniella Sapozhnikova <daniellasapo@gmail.com>
2016-02-16 11:25:28 +0000

    finally done

--- task_04.py
+++ task_04.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """Provides variables for formatting."""
 
-NEWS = 'Hi {friend}! I have {0} news! I won the raffle with number {1:.6f}!'
+NEWS = 'Hi {friend}! I have {0} news! I won the raffle with number {1:06d}!'
 FNAME = 'Pat'
 NTYPE = '*amazing*'
 RNUM = 42
