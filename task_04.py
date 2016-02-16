710712b53e89cb019818b0438c42f9681619dc62
Meriam Setti <mery81@hotmail.com>
2016-02-16 10:38:03 +0000

    Week-03 Synthesizing

--- task_04.py
+++ task_04.py
@@ -2,7 +2,12 @@
 # -*- coding: utf-8 -*-
 """Provides variables for formatting."""
 
-NEWS = 'Hi {friend}! I have {0} news! I won the raffle with number {1}!'
+NEWS = "Hi {}! I have {} news! I won the raffle with number {:0>6}!"
+
 FNAME = 'Pat'
 NTYPE = '*amazing*'
 RNUM = 42
+
+EMAIL = NEWS.format(FNAME, NTYPE, RNUM)
+
+print EMAIL
