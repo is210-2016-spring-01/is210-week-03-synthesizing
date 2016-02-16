09c484d7305ac88ff622fce165ea10bc6d5f493c
Nicole Parrilla <nicole.parrilla@spsmail.cuny.edu>
2016-02-16 00:29:17 +0000

    Changed values to true

--- task_05.py
+++ task_05.py
@@ -26,7 +26,7 @@ def get_member_count(my_sequence):
     try:
         length = len(my_sequence)
     except TypeError:
-        length = False
+        length = True
 
     return length
 
@@ -61,12 +61,12 @@ def is_empty(my_sequence):
     """
     count = get_member_count(my_sequence)
 
-    if count != False:
+    if count != True:
         return count == 0
     else:
         raise TypeError('Object has no len()')
 
 
-TEST = ''
+TEST = 'True'
 print len(TEST)
 print is_empty(TEST)
