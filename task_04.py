50fd9c3e8b0bebd9a2a95b4dfaf7ac1cc4f1d7dc
Vanessa Zuniga <vanessa.zuniga@spsmail.cuny.edugit commit -m added>
2016-02-19 03:32:13 +0000

    formatting a string

--- task_04.py
+++ task_04.py
@@ -2,7 +2,37 @@
 # -*- coding: utf-8 -*-
 """Provides variables for formatting."""
 
-NEWS = 'Hi {friend}! I have {0} news! I won the raffle with number {1}!'
+NEWS = 'Hi {friend}! I have {0} news! I won the raffle with number {1}!'.format('Pat', '*amazing*', '42')
+
 FNAME = 'Pat'
 NTYPE = '*amazing*'
 RNUM = 42
+
+{friend} == FNAME
+{0} == NTYPE
+{1} == RNUM
+EMAIL = 'Hi {friend} ! I have NTYPE news! I won the raffle with number {1:6f}!'.format('Pat', '*amazing*', '42')
+print EMAIL
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
