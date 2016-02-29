5489adac697f2c5f7843754e32a4404a125f9dc5
Elisha Chauhan <elisha.chauhan@spsmail.cuny.edu>
2016-02-29 23:53:01 +0000

    Changed formatting

--- task_04.py
+++ task_04.py
@@ -2,13 +2,9 @@
 # -*- coding: utf-8 -*-
 """Provides variables for formatting."""
 
-NEWS = 'Hi {friend}! I have {0} news! I won the raffle with number {1:0>6}!'
+NEWS = 'Hi {friend}! I have {0} news! I won the raffle with number {1:06}!'
 FNAME = 'Pat'
 NTYPE = '*amazing*'
 RNUM = 42
 
-print NEWS.format(friend='FNAME', 'NTYPE', 'RNUM')
-
-EMAIL = 'Hi Pat! I have *amazing* news! I won the raffle with number 000042!'
-
-print EMAIL
+EMAIL = NEWS.format(NTYPE, RNUM, friend=FNAME)
